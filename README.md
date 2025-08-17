e59feb29be1807ce274accd43c055f0c4604ae5d
minhhoccode111 <minhhoccode111@gmail.com>
2025-08-17 13:40:37 +0700

    add: handle pass-in arguments, read file, handle csv row including edge case

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ Use Golang to parse CSV files and use it to calculate some basic statistics
   Article](https://learncodethehardway.com/blog/36-an-efficient-go-study-guide/)
 - CSV files are generated using [this
   repo](https://github.com/datablist/sample-csv-files)
+
+NOTE: We can use the `encoding/csv` package, which will make our life easier, but for the sake of practicing, we will use `bufio` instead.
+
+Because sometimes, we will have to handle edge cases like this, a column has `,` character in it
+
+```csv
+21,8de40AC4e6EaCa4,"Velez, Payne and Coffey",http://burton.com/,Luxembourg,Mandatory coherent synergy,1986,Wholesale,5010
+```
+
+## Concepts
+
+- Read file
+- Deal with the CSV
+- `strings.SplitSeq()` to return iterator
--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"encoding/csv"
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println(os.Args)
 	args := os.Args
 	if len(args) < 3 {
 		panic("usage: <program> <path> <col>")
 	}
+
 	path := args[1]
-	col := args[2]
+
+	if !strings.HasSuffix(path, ".csv") {
+		s := fmt.Sprintf("Path must have .csv file extension, not %s", path)
+		panic(s)
+	}
+
+	// col := args[2]
+
 	file, err := os.Open(path)
 	if err != nil {
 		s := fmt.Sprintf("Error opening %s: %s", path, err)
@@ -21,9 +30,84 @@ func main() {
 	}
 	defer file.Close()
 
-	// create a new csv reader
-	reader := csv.NewReader(file)
+	scanner := bufio.NewScanner(file)
+	var table map[string][]string
+	var header []string
+	count := 0
 
-	// read all records from csv
+	// read the file line by line
+	for scanner.Scan() {
+		// expect row 0 to be the header
+		if count == 0 {
+			count++
+			headerStr := scanner.Text()
+			headerSlice := strings.Split(headerStr, ",")
+			header = handleRow(headerSlice)
+			table = make(map[string][]string, len(headerSlice))
+			for _, col := range headerSlice {
+				table[col] = []string{}
+			}
+			continue
+		}
 
+		count++
+		rowStr := scanner.Text()
+		rowSlice := strings.Split(rowStr, ",")
+		rowSlice = handleRow(rowSlice)
+
+		// panic if current row's length not equal to the header
+		if len(header) != len(rowSlice) {
+			s := fmt.Sprintf("Expected to have %v columns but got %v", len(header), len(rowSlice))
+			panic(s)
+		}
+
+		// add to map with correct order
+		for i, col := range rowSlice {
+			rowHeader := header[i]
+			table[rowHeader] = append(table[rowHeader], col)
+		}
+	}
+
+	for k, v := range table {
+		fmt.Println(k)
+		fmt.Println(v)
+	}
+
+	// if error is not EOF, then panic the error
+	if err := scanner.Err(); err != nil && err != io.EOF {
+		s := fmt.Sprintf("Error reading %s: %s", path, err)
+		panic(s)
+	}
+}
+
+// handleRow makes sure we handle each row in csv file correct
+func handleRow(cols []string) []string {
+	newCols := []string{}
+	var curr string
+	for _, col := range cols {
+		// start of special column
+		if strings.HasPrefix(col, "\"") {
+			curr = col[1:]
+			continue
+		}
+
+		// in the middle of a special column
+		if curr != "" {
+			// it's the end
+			if strings.HasSuffix(col, "\"") {
+				curr += col[:len(col)-1]
+				newCols = append(newCols, curr)
+				curr = ""
+				continue
+			}
+
+			// it's the middle
+			curr += col
+			continue
+		}
+
+		// normal column
+		newCols = append(newCols, col)
+	}
+	return newCols
 }
